6f7eee82f2c6f7db07afdb6c73253a9b707ea35b
fhillipbagsic <75466516+fhillipbagsic@users.noreply.github.com>
2021-11-02 16:28:05 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Container from "./Components/Container";
+import Controls from "./Components/ControlComponents/Controls";
+import NavBar from "./Components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Controls />
+      <Container />
+    </>
+  );
+}
+
+export default App;
